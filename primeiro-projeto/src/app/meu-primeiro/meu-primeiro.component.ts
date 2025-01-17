d883405060b3b2b6bd0dc9deb4a5e5415b28b818
Kamylle <sharmankamylle@gmail.com>
2025-01-17 00:51:15 -0300

    estrutura basica do angulkar e ts

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro',
+  imports: [],
+  templateUrl: './meu-primeiro.component.html',
+  styleUrl: './meu-primeiro.component.css'
+})
+export class MeuPrimeiroComponent {
+
+}
