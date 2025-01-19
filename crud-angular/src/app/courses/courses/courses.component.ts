b4295f28e7cf5a5d27b934dec9c57d737749d547
Kamylle <sharmankamylle@gmail.com>
2025-01-18 23:25:05 -0300

    creating a table to list registered courses

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: false,
-  
+
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
 
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
